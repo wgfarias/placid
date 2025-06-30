a73264205dea5adea6b78b001f6a81d12b11a54a
Williams Gonçalves de Farias <wgfarias@gmail.com>
2025-06-30 08:24:41 -0300

    config: Adicionar variáveis de ambiente SNDT no ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,11 @@ module.exports = {
         PORT: 3000,
         API_KEY: "placid_secret_key", // Altere para uma chave segura em produção
         STORAGE_PATH: "./src/public/images",
+        // Configurações do upload para SNDT
+        UPLOAD_TO_SNDT: "true",
+        DELETE_LOCAL_AFTER_UPLOAD: "true",
+        SNDT_API_TOKEN: "sndt_live_abc123def456", // Altere para o token real em produção
+        SNDT_API_URL: "https://files.sndt.com.br/placid/api/upload-base64.php",
       },
     },
   ],
